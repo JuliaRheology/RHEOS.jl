70e01569e2a48a0d7b5c557427846eaa1cd09d84
Louis Kaplan <louis.kaplan@slcu.cam.ac.uk>
2018-02-27 14:04:52 +0000

    updated example1 doc structure

--- examples/example1.jl
+++ examples/example1.jl
@@ -1,28 +1,31 @@
 #!/usr/bin/env julia
 include("../src/RHEOS.jl")
 using RHEOS
+using PyPlot
 
 filedir = "../data/rheologyData1.csv"
 
-data = fileload(filedir, ["time","stress","strain"])
+data_raw = fileload(filedir, ["time","stress","strain"], "strlx")
 
-# data.insight = true
+# data_raw.insight = true
 
-fixed_resample!(data, [1,450],[8],["up"])
-# downsample!(data, [1,450], [2])
-# var_resample!(data, :σᵦ, 0.1, _mapback = true)
+# data_resampled = var_resample(data_raw, :σ, 0.1, _mapback = true)
+# data_resampled = downsample(data_raw, [1,450,500], [2,1])
+data_resampled = fixed_resample(data_raw, [1,450],[8],["up"])
+
+data_resampled.insight = true
 
 # SLS fit
 lb = [0.0, 0.0, 0.0]
 ub = [1e5, 1e5, 1e5]
 p0 = [1000.0, 1000.0, 100.0]
-modelfit!(data, "strlx", "SLS", p0, lb, ub)
+modelfit!(data_resampled, "SLS", p0, lb, ub)
 
 # Spring-pot fit
 lb = [0.0, 0.0]
 ub = [1e5, 1.0]
 p0 = [1000.0, 0.5]
-modelfit!(data, "strlx", "springpot", p0, lb, ub; singularity = true)
+modelfit!(data_resampled, "springpot", p0, lb, ub; singularity = true)
 
-fiteval(data, "SLS", "strlx")
-fiteval(data, "springpot", "strlx"; singularity = true)
+fiteval(data_resampled, "SLS")
+fiteval(data_resampled, "springpot"; singularity = true)
