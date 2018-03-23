d5c4cd5f7875d4198f0407a5d572a76afcc0585f
Louis Kaplan <louis.kaplan@slcu.cam.ac.uk>
2018-03-23 18:55:02 +0000

    hertz working

--- examples/scratchpad.jl
+++ examples/scratchpad.jl
@@ -1,13 +1,30 @@
-function callbelow()
+mutable struct Happy
+    intensity::Int
+    reason::String
+end
 
-    tobecalled()
+mutable struct Sad
+    intensity::Int
+    reason::String
+    howlong::Int
+end
+
+Emotion = Union{Happy, Sad}
+
+function emotion_reader(self::Emotion)
+
+    println(typeof(self))
+    println("Intensity: ", self.intensity)
+    println("Reason: ", self.reason)
 
 end
 
-function tobecalled()
+a_happy_emotion = Happy(10, "great day")
 
-    println(mean([1,2,3,3,3]))
+a_sad_emotion = Sad(7, "bad tea", 2)
 
-end
+emotion_reader(a_happy_emotion)
 
-callbelow()
+println("\n")
+
+emotion_reader(a_sad_emotion)
