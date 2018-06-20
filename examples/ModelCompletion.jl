10267699e3bee1024e6e80db3dd889c5929044dd
Louis Kaplan <louis.kaplan@slcu.cam.ac.uk>
2018-06-20 14:49:14 +0100

    preprocessing halfway revised

--- examples/ModelCompletion.jl
+++ examples/ModelCompletion.jl
@@ -7,11 +7,21 @@ filedir = "../data/rheologyData1_incomplete.csv"
 
 data_partial = fileload(["time", "stress"], filedir)
 
-# plot init
-fig, ax = subplots()
+# data_resampled = var_resample(data_partial, :σ, 0.1; _mapback = false)
+data_resampled = downsample(data_partial, [1, 450], [3])
 
-# plot stress data
-ax[:plot](data_partial.t, data_partial.σ)
+println(data_resampled.sampling)
+println(data_resampled.log)
+
+plot(data_partial.t, data_partial.σ, "x")
+plot(data_resampled.t, data_resampled.σ, "o")
+show()
+
+# # plot init
+# fig, ax = subplots()
+
+# plot stress data to check loaded in properly
+# ax[:plot](data_partial.t, data_partial.σ)
 
 # # get SLS data and plot strain data
 # modelcomplete!(data_partial, "SLS", [844.152, 2043.98, 5.15527])
@@ -21,6 +31,6 @@ ax[:plot](data_partial.t, data_partial.σ)
 # modelcomplete!(data_partial, "springpot", [2997.7, 0.281836])
 # ax[:plot](data_partial.t[2:end], data_partial.measured, label="springpot")
 
-# legend and plot
-ax[:legend](loc="best")
-show()
+# # legend and plot
+# ax[:legend](loc="best")
+# show()
--- examples/StandardFitting-MultipleModels.jl
+++ examples/StandardFitting-MultipleModels.jl
@@ -7,9 +7,20 @@ filedir = "../data/rheologyData1.csv"
 
 data_raw = fileload(["time","stress","strain"], filedir)
 
+# data_resampled = var_resample(data_raw, :σ, 0.1; _mapback = false)
+# data_resampled = downsample(data_raw, [1, 450], [3])
+data_resampled = downsample(data_raw, [1, 450], [3])
+
+println(data_resampled.sampling)
+println(data_resampled.log)
+
+plot(data_raw.t, data_raw.σ, "x")
+plot(data_resampled.t, data_resampled.σ, "o")
+show()
+
 # data_resampled = fixed_resample(data_raw, [1, 450],[8],["up"])
 # data_resampled = smooth(data_resampled, 5.0)
-# data_resampled = var_resample(data_resampled, :σ, 0.1, _mapback = false)
+
 # data_resampled = mapbackdata(data_resampled, data_raw)
 
 # # SLS fit
