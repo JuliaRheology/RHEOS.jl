0cfd5f4af4e499dd162f3c17164c4f8f18422a3c
Louis Kaplan <louis.kaplan@slcu.cam.ac.uk>
2018-06-26 14:43:01 +0100

    proc partial almost done

--- examples/StandardFitting-MultipleModels.jl
+++ examples/StandardFitting-MultipleModels.jl
@@ -26,13 +26,13 @@ data_resampled = fixed_resample(data_raw, [1, 450], [8], ["up"])
 p0 = [1000.0, 1000.0, 100.0]
 lb = [0.0, 0.0, 0.0]
 ub = [1e5, 1e5, 1e5]
-fittedmodel = modelfit(data_resampled, G_SLS; p0=p0, lo=lb, hi=ub, verbose=true)
+sls_fit = modelfit(data_resampled, G_SLS; p0=p0, lo=lb, hi=ub, verbose=false)
 
-# # Spring-pot fit
-# p0 = [1000.0, 0.5]
-# lb = [0.0, 0.0]
-# ub = [1e5, 1.0]
-# modelfit!(data_resampled, "springpot", p0, lb, ub)
+# Spring-pot fit
+p0 = [1000.0, 0.5]
+lb = [0.0, 0.0]
+ub = [1e5, 1.0]
+springpot_fit = modelfit(data_resampled, G_springpot; p0=p0, lo=lb, hi=ub, verbose=false)
 
 # # Fract Special fit
 # p0 = [247.0, 6.48e2, 0.25, 4.26e3]
@@ -46,7 +46,7 @@ fittedmodel = modelfit(data_resampled, G_SLS; p0=p0, lo=lb, hi=ub, verbose=true)
 
 # fiteval(data_resampled)
 
-# saveresult(data_resampled; include_data = true)
+# saveresult(data_resampled; include_data = false)
 # saveresult(data_resampled; include_data = false)
 
 # alldatasaved = loadresult("../data/rheologyData1_RheologyData.jld")
