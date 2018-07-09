3f10a311152d32512346421862d4c3116f3de7d5
Louis Kaplan <louis.kaplan@slcu.cam.ac.uk>
2018-07-09 19:52:19 +0100

    update REQUIRE and make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,16 @@
-push!(LOAD_PATH, "../src/")
-
 using Documenter, RHEOS
 
-makedocs()
+makedocs(modules=["RHEOS"],
+         doctest=false, clean=true,
+         format =:html,
+         sitename="RHEOS.jl",
+         authors="Louis Kaplan, Alessandra Bonfanti, Alexandre Kabla"
+         )
+
+deploydocs(repo = "github.com:rheos-cambridge/RHEOS.jl.git",
+           target = "build",
+           osname = "windows",
+           julia = "0.6.3",
+           deps = nothing,
+           make = nothing
+           )
