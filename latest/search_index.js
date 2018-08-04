610d1c30016278acb7821b5dce6de12741570408
autodocs <autodocs>
2018-08-04 19:03:26 +0000

    build based on a5b2d56

---
+++ latest/search_index.js
@@ -0,0 +1,27 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#RHEOS.jl-Documentation-1",
+    "page": "Home",
+    "title": "RHEOS.jl Documentation",
+    "category": "section",
+    "text": "RHEOS, an abbreviation of Rheology Open Source, is a Julia package which provides tools for analysis of rheology data and generation of new viscoelastic models. It features a large array of standard and fractional linear viscoelastic models - new ones can be easily added by the user. It can also be used as an educational tool to demonstrate the qualitative behaviour of various viscoelastic models. "
+},
+
+{
+    "location": "index.html#Test-subtitle-1",
+    "page": "Home",
+    "title": "Test subtitle",
+    "category": "section",
+    "text": ""
+},
+
+]}
