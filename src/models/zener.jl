8b9331f2b4f3b68d39ae90fd95cc15e5fbfd7b0d
Louis Kaplan <louis.kaplan@slcu.cam.ac.uk>
2018-10-05 12:46:44 +0100

    fix NaN InverseLaplace issue for FractionalZener and FractionalSLS

--- src/models/zener.jl
+++ src/models/zener.jl
@@ -12,9 +12,11 @@ function J_fraczener(t::Array{T,1}, params::Array{T,1}) where T<:Real
 
     Jbar(s) = (1/s)*(cₐ*s^a + cᵦ*s^β)/(cₐ*s^a*cᵦ*s^β + cᵧ*s^γ*(cₐ*s^a + cᵦ*s^β))
 
-    J = InverseLaplace.ILt(s -> Jbar(s))
+    # method gives NaN at time 0.0
+    # J = InverseLaplace.ILt(s -> Jbar(s))
+    # return [J(t_val) for t_val in t]
 
-    return [J(t_val) for t_val in t]
+    return InverseLaplace.talbotarr(s -> Jbar(s), t)
 end
 
 function Gp_fraczener(ω::Array{T,1}, params::Array{T,1}) where T<:Real
@@ -51,10 +53,12 @@ function J_fracsls(t::Array{T,1}, params::Array{T,1}) where T<:Real
     cₐ, a, kᵦ, kᵧ = params
 
     Jbar(s) = (1/s)*(cₐ*s^a + kᵦ)/(cₐ*s^a*kᵦ + kᵧ*(cₐ*s^a + kᵦ))
+    
+    # method gives NaN at time 0.0
+    # J = InverseLaplace.ILt(s -> Jbar(s))
+    # return [J(t_val) for t_val in t]
 
-    J = InverseLaplace.ILt(s -> Jbar(s))
-
-    return [J(t_val) for t_val in t]
+    return InverseLaplace.talbotarr(s -> Jbar(s), t)
 end
 
 function Gp_fracsls(ω::Array{T,1}, params::Array{T,1}) where T<:Real
@@ -65,7 +69,6 @@ function Gp_fracsls(ω::Array{T,1}, params::Array{T,1}) where T<:Real
     numerator = kᵦ^2*(cₐ*ω.^a)*cos(a*π/2) + ((cₐ*ω.^a).^2)*kᵦ
 
     Gp = numerator./denominator + kᵧ
-
 end
 
 function Gpp_fracsls(ω::Array{T,1}, params::Array{T,1}) where T<:Real
@@ -76,7 +79,6 @@ function Gpp_fracsls(ω::Array{T,1}, params::Array{T,1}) where T<:Real
     numerator = kᵦ^2*(cₐ*ω.^a)*sin(a*π/2)
 
     Gpp = numerator./denominator 
-
 end
 
 FractionalSLS() = RheologyModel(G_fracsls, J_fracsls, Gp_fracsls, Gpp_fracsls, [0.5, 0.5, 1.0, 0.2], ["model created with default parameters"])
@@ -108,7 +110,6 @@ function Gp_sls(ω::Array{T,1}, params::Array{T,1}) where T<:Real
     numerator = ω.^2*τ^2*kᵦ
 
     Gp = numerator./denominator + kᵧ
-
 end
 
 function Gpp_sls(ω::Array{T,1}, params::Array{T,1}) where T<:Real
@@ -120,7 +121,6 @@ function Gpp_sls(ω::Array{T,1}, params::Array{T,1}) where T<:Real
     numerator = ω*τ*kᵦ
 
     Gpp = numerator./denominator
-
 end
 
 SLS() = RheologyModel(G_sls, J_sls, Gp_sls, Gpp_sls, [1.0, 0.5, 1.0], ["model created with default parameters"])
