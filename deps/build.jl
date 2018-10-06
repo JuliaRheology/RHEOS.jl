f7283bd0d3df07016562c4d284d11fed52af644e
Louis Kaplan <louis.kaplan@slcu.cam.ac.uk>
2018-10-06 15:29:29 +0100

    deps unneccesary

--- deps/build.jl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env julia
-
-println("Importing Pkg...\n")
-import Pkg
-println("Pkg Import Successful.\n")
-
-# Pkg.add("https://github.com/JuliaRheology/MittLeffLite.jl")
-# Pkg.add("https://github.com/JuliaRheology/FastConv.jl")
