8cc6770a7867be03feed10624622d3617f597180
autodocs <autodocs>
2018-10-07 17:07:12 +0000

    build based on b4cf3d9

--- latest/search_index.js
+++ latest/search_index.js
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Installation",
     "category": "section",
-    "text": "From within the Julia v0.6.3 REPL, type:Pkg.clone(\"https://github.com/rheos-cambridge/RHEOS.jl.git\")Then, from within the RHEOS directory in your Julia library (or just downloaded from the Github seperately), run:julia TEMP_INSTALL.jlto install the two dependencies not yet registered on METADATA."
+    "text": "Install Julia, version 1.0.1\nFrom Julia REPL, enter pkg mode by pressing ]\nRun the command add \"https://github.com/JuliaRheology/RHEOS.jl\""
 },
 
 {
@@ -73,35 +73,35 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "preprocessing.html#RHEOS.var_resample-Tuple{RHEOS.RheologyData,Symbol,Float64}",
+    "location": "preprocessing.html#RHEOS.var_resample-Tuple{RheologyData,Symbol,Float64}",
     "page": "Preprocessing",
     "title": "RHEOS.var_resample",
     "category": "method",
-    "text": "var_resample(self::RheologyData, refvar::Symbol, pcntdownsample::Float64; mapback::Bool = false)\n\nConvert a fixed sample rate array to a variable sample rate, with sampling points added according to a relative change in chosen variable refvar, 1st derivative of refvar and 2nd derivative of refvar (WRT time). Usually chosen as the measured variable, so :σ for a stress relaxation test and :ϵ for a creep test.\n\nCurrently only variable downsampling supported. pcntdown sample is approximate, works well in some cases and very poorly in others. If required, compare resampled length vs original length after processing has finished. If data is noisy, may benefit from sending smoothed signal to this algorithm and either using mapback function or interpolating onto unsmoothed data.\n\nSee help docstring for var_resample for more details on algorithm implementation.\n\n\n\n"
+    "text": "var_resample(self::RheologyData, refvar::Symbol, pcntdownsample::Float64; mapback::Bool = false)\n\nConvert a fixed sample rate array to a variable sample rate, with sampling points added according to a relative change in chosen variable refvar, 1st derivative of refvar and 2nd derivative of refvar (WRT time). Usually chosen as the measured variable, so :σ for a stress relaxation test and :ϵ for a creep test.\n\nCurrently only variable downsampling supported. pcntdown sample is approximate, works well in some cases and very poorly in others. If required, compare resampled length vs original length after processing has finished. If data is noisy, may benefit from sending smoothed signal to this algorithm and either using mapback function or interpolating onto unsmoothed data.\n\nSee help docstring for var_resample for more details on algorithm implementation.\n\n\n\n\n\n"
 },
 
 {
-    "location": "preprocessing.html#RHEOS.downsample-Tuple{RHEOS.RheologyData,Array{Float64,1},Array{Int64,1}}",
+    "location": "preprocessing.html#RHEOS.downsample-Tuple{RheologyData,Array{Float64,1},Array{Int64,1}}",
     "page": "Preprocessing",
     "title": "RHEOS.downsample",
     "category": "method",
-    "text": "downsample(self::RheologyData, time_boundaries::Array{Float64,1}, elperiods::Array{Int64,1})\n\nHigh-level RheologyData interface to downsample in base.jl. Boundaries are floating point times which are then converted to the closest elements.\n\n\n\n"
+    "text": "downsample(self::RheologyData, time_boundaries::Array{Float64,1}, elperiods::Array{Int64,1})\n\nHigh-level RheologyData interface to downsample in base.jl. Boundaries are floating point times which are then converted to the closest elements.\n\n\n\n\n\n"
 },
 
 {
-    "location": "preprocessing.html#RHEOS.fixed_resample-Tuple{RHEOS.RheologyData,Array{Float64,1},Array{Int64,1},Array{String,1}}",
+    "location": "preprocessing.html#RHEOS.fixed_resample-Tuple{RheologyData,Array{Float64,1},Array{Int64,1},Array{String,1}}",
     "page": "Preprocessing",
     "title": "RHEOS.fixed_resample",
     "category": "method",
-    "text": "fixed_resample(self::RheologyData, time_boundaries::Array{Float64,1}, elperiods::Array{Int64,1}, direction::Array{String,1})\n\nHigh-level RheologyData interface to fixed_resample in base.jl\n\n\n\n"
+    "text": "fixed_resample(self::RheologyData, time_boundaries::Array{Float64,1}, elperiods::Array{Int64,1}, direction::Array{String,1})\n\nHigh-level RheologyData interface to fixed_resample in base.jl\n\n\n\n\n\n"
 },
 
 {
-    "location": "preprocessing.html#RHEOS.smooth-Tuple{RHEOS.RheologyData,Float64}",
+    "location": "preprocessing.html#RHEOS.smooth-Tuple{RheologyData,Float64}",
     "page": "Preprocessing",
     "title": "RHEOS.smooth",
     "category": "method",
-    "text": "smooth(self::RheologyData, τ::Float64)\n\nSmooth data using a Gaussian Kernel to time scale τ (approximately half power).\n\nSmooths both σ and ϵ.\n\n\n\n"
+    "text": "smooth(self::RheologyData, τ::Float64)\n\nSmooth data using a Gaussian Kernel to time scale τ (approximately half power).\n\nSmooths both σ and ϵ.\n\n\n\n\n\n"
 },
 
 {
@@ -113,59 +113,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "fitpredict.html#RHEOS.modelpredict-Tuple{RHEOS.RheologyData,RHEOS.RheologyModel}",
-    "page": "Fit and Predict",
-    "title": "RHEOS.modelpredict",
-    "category": "method",
-    "text": "modelpredict(data::RheologyData, model::RheologyModel)\n\nGiven data and model and parameters, predict new dataset based on both.\n\n\n\n"
-},
-
-{
     "location": "fitpredict.html#",
     "page": "Fit and Predict",
     "title": "Fit and Predict",
     "category": "page",
     "text": "modelfit(data::RheologyData, modulus::Function[, p0::Array{Float64,1}, lo::Array{Float64,1}, hi::Array{Float64,1}; verbose::Bool = false])modelpredict(data::RheologyData, model::RheologyModel)"
-},
-
-{
-    "location": "loadsave.html#RHEOS.savedata-Tuple{RHEOS.RheologyData}",
-    "page": "Save and Load",
-    "title": "RHEOS.savedata",
-    "category": "method",
-    "text": "savedata(self::RheologyData; filedir::String = \"\", ext = \"_RheologyData.jld\")\n\nSave RheologyData object using JLD format. Save file directory must be specified. If data was loaded from disk using fileload or the full RheologyData constructor then filedir argument can  be set to empty string \"\" which will try to use the original file  dir concatenated with the optional ext string argument  - e.g.  \"/originalpathto/file.csv_RheologyData.jld\".\n\n\n\n"
-},
-
-{
-    "location": "loadsave.html#RHEOS.loaddata-Tuple{String}",
-    "page": "Save and Load",
-    "title": "RHEOS.loaddata",
-    "category": "method",
-    "text": "loaddata(filedir::String)\n\nConvenience function loads RheologyData.\n\n\n\n"
-},
-
-{
-    "location": "loadsave.html#RHEOS.savemodel-Tuple{RHEOS.RheologyModel}",
-    "page": "Save and Load",
-    "title": "RHEOS.savemodel",
-    "category": "method",
-    "text": "savemodel(self::RheologyModel; filedir::String = \"\", ext = \"\")\n\n\n\n"
-},
-
-{
-    "location": "loadsave.html#RHEOS.loadmodel-Tuple{String}",
-    "page": "Save and Load",
-    "title": "RHEOS.loadmodel",
-    "category": "method",
-    "text": "loadmodel(filedir::String)\n\nConvenience function loads RheologyModel from disk.\n\n\n\n"
-},
-
-{
-    "location": "loadsave.html#RHEOS.exportdata-Tuple{RHEOS.RheologyData}",
-    "page": "Save and Load",
-    "title": "RHEOS.exportdata",
-    "category": "method",
-    "text": "exportdata(self::RheologyData; filedir::String = \"\", ext = \"_mod.csv\")\n\nExport RheologyData to csv format. Exports three columns in order: stress, strain, time. Useful for plotting/analysis in other software.\n\n\n\n"
 },
 
 {
