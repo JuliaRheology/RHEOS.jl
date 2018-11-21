448465674ec190df44986f0727588292bdb0c835
Louis Kaplan <louis.kaplan@slcu.cam.ac.uk>
2018-11-21 15:55:38 +0000

    remove smoothgauss test

--- test/base.jl
+++ test/base.jl
@@ -37,21 +37,6 @@ end
 @test RHEOS.sampleratecompare([1.0, 2.0, 3.0], [2.0, 3.0, 4.0])==true
 @test RHEOS.sampleratecompare([1.0, 2.0, 3.0], [0.1, 0.2, 0.3])==false
 
-function _smoothgauss(tol)
-    x = collect(0.0:0.01:30)
-
-    f = 0.2
-    y = sin.(2π*f*x)
-
-    ys = RHEOS.smoothgauss(y, x, 1/f; pad="circular")
-
-    reduced_power = 0.5642720830237535
-    ytest = reduced_power*sin.(2π*f*x)
-
-    all(i -> (ys[i]-ytest[i])<tol, 1:length(x))
-end
-@test _smoothgauss(tol)
-
 # @test _var_resample()
 
 @test RHEOS.closestindex([1.0, 2.0, 3.0], 1.7)==2
