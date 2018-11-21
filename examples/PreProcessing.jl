f256e6f64495c3fc38daea5ea6f4dcf6c24f2f2d
Louis Kaplan <louis.kaplan@slcu.cam.ac.uk>
2018-11-21 13:52:35 +0000

    fixed interpolations dynamic loading

--- examples/PreProcessing.jl
+++ examples/PreProcessing.jl
@@ -1,17 +1,19 @@
 #!/usr/bin/env julia
-# using PyPlot
-using RHEOS
+# using RHEOS
 
 filedir = "DataComplete.csv"
 
 # repeated step loading generated with FractionalSLS([2.0, 0.5, 0.5, 0.7])
-data = fileload(["stress","strain", "time"], filedir)
+data = importdata(["stress","strain", "time"], filedir)
 
-data_smoothed = smooth(data, 5.0)
+# data_smoothed = smooth(data, 5.0)
 
-# data_resampled = var_resample(data_raw, :σ, 0.1; _mapback = false)
+data_resampled = var_resample(data, :σ, 0.1; _mapback = false)
+
 # data_resampled = downsample(data_raw, [0.0, 40.0], [3])
+
 # data_resampled = fixed_resample(data_raw, [0.0, 20.0, 41.0], [8, 25], ["up", "down"])
+
 # data_resampled = fixed_resample(data_raw, [0.0, 40.0], [8], ["up"])
 
 # data_resampled = mapbackdata(data_resampled, data_raw)
