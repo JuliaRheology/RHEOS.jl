ba4e5773c875f20302dcd72d2761bb02fdc2787a
autodocs <autodocs>
2018-11-23 02:02:33 +0000

    build based on 28582fa

--- dev/search_index.js
+++ dev/search_index.js
@@ -117,7 +117,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Predicting Responses",
     "title": "Predicting Responses",
     "category": "section",
-    "text": ""
+    "text": "SLS + Fractional SLS normal dataFractional SLS frequency data"
 },
 
 {
@@ -166,6 +166,22 @@ var documenterSearchIndex = {"docs": [
     "title": "File I/O",
     "category": "section",
     "text": ""
+},
+
+{
+    "location": "fileIO/#CSV-Import/Export-1",
+    "page": "File I/O",
+    "title": "CSV Import/Export",
+    "category": "section",
+    "text": "In the Fitting Data section we saw how to import data from csv files. As a brief overview of that functionality, the 3 lines below demonstrate how to import stress/strain/time data, partial strain/time data, and G\'/G\'\'/frequency data. Note that if you want to try and run this code exactly as shown below, the data files used in these examples are stored in the \'RHEOS/examples\' directory.data = importdata([\"stress\",\"strain\", \"time\"], \"DataComplete.csv\")\n\ndata_incomplete = importdata([\"strain\", \"time\"], \"DataIncomplete.csv\")\n\ndata_dynamic = importdata([\"Gp\",\"Gpp\", \"Frequency\"], \"FrequencyData.csv\")Where the first two function calls return RheologyData objects and the last function call returns a RheologyDynamic object.If you want to analyse or plot your data in software other than Julia you will likely want to export it to a CSV file. To export RheologyData and RheologyDynamic objects to CSV files we can use the exportdata function. For the two complete data-sets we imported above, we can export them into new files in the following way.exportdata(data, \"exported_data\")\n\nexportdata(data_dynamic, \"exported_dynamic_data\")The \'.csv\' extension will automatically be added (and can be modified if needs be through use of a keyword argument). The delimiter can also be changed if necessary. See API section for information on this."
+},
+
+{
+    "location": "fileIO/#Native-Julia-data-files-(JLD2)-1",
+    "page": "File I/O",
+    "title": "Native Julia data files (JLD2)",
+    "category": "section",
+    "text": "When using RHEOS and doing all subsequent analysis and plotting in Julia, it is convenient to be able to store native Julia objects to disk so that they can be loaded in to subsequent Julia sessions. RHEOS provides two pairs of convenience functions to facilitate this: savedata and loaddata, savemodel and loadmodel. Using our data imported above we can demonstrate saving and loading data (it is exactly the same process for both RHEOS data types).savedata(data, \"imported_data\")\n\nloaded_data = loaddata(\"imported_data.jld2\")The savedata function will automatically append a \'.jld2\' to the filename when saving but this can be modified if necessary through a keyword argument.Lastly, let\'s create a Standard Linear Solid model object with default parameters, save that to disk and load it again.model = SLS()\n\nsavemodel(model, \"saved_model\")\n\nloaded_model = loadmodel(\"saved_model.jld2\")"
 },
 
 {
@@ -606,22 +622,6 @@ var documenterSearchIndex = {"docs": [
     "title": "File I/O",
     "category": "section",
     "text": "importdata\nexportdata\nsavedata\nloaddata\nsavemodel\nloadmodel"
-},
-
-{
-    "location": "moreexamples/#",
-    "page": "More Examples",
-    "title": "More Examples",
-    "category": "page",
-    "text": ""
-},
-
-{
-    "location": "moreexamples/#More-Examples-1",
-    "page": "More Examples",
-    "title": "More Examples",
-    "category": "section",
-    "text": "The following examples combine multiple elements of RHEOS into scripts which are likely to resemble common workflows. In addition to RHEOS, some of them use the PyPlot package for plotting. All of the examples below are also stored in the RHEOS directory in the \'examples\' folder."
 },
 
 ]}
