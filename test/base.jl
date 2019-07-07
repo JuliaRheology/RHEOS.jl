5b6935112a93f520dd4b6418cc878c743296d0d8
Louis Kaplan <louis.kaplan@slcu.cam.ac.uk>
2019-07-07 18:50:02 +0100

    remove redundant tests, make tol a constant

--- test/base.jl
+++ test/base.jl
@@ -1,4 +1,4 @@
-tol = (eps(RHEOS.RheoFloat))^(0.125) 
+const tol = (eps(RHEOS.RheoFloat))^(0.125) 
 
 function _trapz(tol)
     x = [i for i in -5.0:0.01:5.0]
@@ -25,22 +25,16 @@ function _derivBD(tol)
     dy_numeric = RHEOS.derivBD(y, x)
     # note that the backwards difference method has a weaker
     # numerical test than derivCD as it is less accurate
-    all(i -> (dy[i]-dy_numeric[i])<tol, eachindex(dy_numeric))
+    all(i -> isapprox(dy[i], dy_numeric[i], atol=tol), eachindex(dy_numeric))
 end
 @test _derivBD(tol)
 
 @test RHEOS.constantcheck([1.0, 2.0, 3.0])==true
 @test RHEOS.constantcheck([1.0, 2.0, 4.0])==false
 
-@test RHEOS.sampleratecompare([1.0, 2.0, 3.0], [2.0, 3.0, 4.0])==true
-@test RHEOS.sampleratecompare([1.0, 2.0, 3.0], [0.1, 0.2, 0.3])==false
-
 @test RHEOS.closestindex([1.0, 2.0, 3.0], 1.7)==2
 
 @test RHEOS.closestindices([1.0, 2.0, 3.0], [1.7, 3.4])==[2, 3]
-
-@test RHEOS.mapback([1.0, 1.23, 1.24, 1.5], [1.0, 1.1, 1.2, 1.3, 1.4, 1.5])==[1, 3, 6]
-@test RHEOS.mapback([1.0, 1.23, 1.24, 1.5], [1.0, 1.1, 1.2, 1.3, 1.4, 1.5]; return_indices=false)==[1.0, 1.2, 1.5]
 
 @test RHEOS.getsigma(10.0, 1000.0)≈1873.9062512927758
 
