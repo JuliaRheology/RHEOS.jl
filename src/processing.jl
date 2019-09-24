8056b59d2469b36c562256a2f99b620a160da016
Louis Kaplan <louis.kaplan@slcu.cam.ac.uk>
2019-09-24 21:59:28 +0100

    update docstring for indexweight

--- src/processing.jl
+++ src/processing.jl
@@ -10,7 +10,7 @@ Preprocessing functions
 
 Resample data with new sample rate(s).
 
-Resample can downsample or upsample data. If the number of `elperiods` is negative it is going to reduce the number of samples,
+`resample` can downsample or upsample data. If the number of `elperiods` is negative it is going to reduce the number of samples,
 vice versa if it is positive. If `time_boundaries` are not specified, resampling is applied to the whole set of data.
 If number of elements per period (`elperiods`) is `1` or `-1` it returns the original `RheoTimeData`, whilst `0` is not accepted as a valid
 argument for `elperiods`.
@@ -52,6 +52,14 @@ end
 
 This function returns array indices (i.e. an array of integers) which can be sent to the `modelfit` function
 to provide a weighted fitting whilst maintaining constant sample-rate.
+
+`indexweight` can underweight indices or overweight them. If `elperiods` in a given boundary is negative, every `abs(n)` index will be used, where `n` is the `elperiod` 
+corresponding to a given boundary. If `n` is positive, then indicies will be duplicated `n` times such that they are given a higher weighting during the fitting procedure.
+If `time_boundaries` are not specified, resampling is applied to the whole set of data. If number of elements per period (`elperiods`) is `1` or `-1` it returns the original 
+indicies for that boundary, whilst `0` is not accepted as a valid argument for `elperiods`.
+
+The last element may or may not be included. By default the last element is forced to be included
+but this can be negated by providing the keyword argument `includelastel=false`.
 """
 function indexweight(self::RheoTimeData, elperiods::Union{Vector{K}, K}; time_boundaries::Union{Nothing, Vector{T}} = nothing, includelast=true) where {K<:Integer,T<:Real}
     # get element-wise boundaries
