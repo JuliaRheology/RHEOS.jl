ec809e0cb66b1880f366997584897ef660ca2e4c
Louis Kaplan <louis.kaplan@slcu.cam.ac.uk>
2019-10-19 16:02:50 +0100

    add PT test files

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,3 +11,7 @@ include("IO.jl")
 include("processing.jl")
 
 include(joinpath(@__DIR__, "models", "elements.jl"))
+include(joinpath(@__DIR__, "models", "maxwell.jl"))
+include(joinpath(@__DIR__, "models", "kelvinvoigt.jl"))
+include(joinpath(@__DIR__, "models", "zener.jl"))
+include(joinpath(@__DIR__, "models", "poynting-thomson.jl"))
