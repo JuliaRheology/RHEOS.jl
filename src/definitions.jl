2296b98d1d798d60856c9cea4504e87e4533aee9
Alexandre Kabla <ajk61@cam.ac.uk>
2020-05-25 14:22:53 +0100

    update docstring for relaxmod

--- src/definitions.jl
+++ src/definitions.jl
@@ -698,22 +698,7 @@ end
 # These are functions enabling users to access the different moduli functions for each model
 #
 
-"""
-    relaxmod(m::RheoModel, time (single value or array))
-    relaxmod(m::RheoModelClass, time (single value or array), parameters (array, named tupple or keyword parameters))
-    
-    This function provides access to the relaxation modulus of a given model m. It can be used with a broad range of inputs.
-    
-    examples:
-    
-    relaxmod(Maxwell, 1, k=1., η=1)
-  
-    relaxmod(Maxwell, [1,2,3], [1,1])
-    
-    m = RheoModel(Maxwell, k=1., η=1)
-    g = relaxmod(m)
-    g([0,1,2])
-"""
+
 
 
 # julia> relaxmod(Maxwell, 1, )
@@ -738,6 +723,34 @@ end
  
 
 
+
+
+"""
+    The function relaxmod provides access to the relaxation modulus (G) of a given model m. It can be used with a broad range of inputs.
+
+    - When a time value is provided (or an array of time values), the function returns the corresponding value(s) of the relaxation modulus.
+    
+    relaxmod(m::RheoModel, time (single value or array))
+    relaxmod(m::RheoModelClass, time (single value or array), parameters (array, named tupple or keyword parameters))
+    
+    examples:
+    
+    relaxmod(Maxwell, 1, k=1., η=1)
+  
+    relaxmod(Maxwell, [1,2,3], [1,1])
+
+
+    - When no time value is provided, relaxmod returns the relaxation function itself.
+    
+    relaxmod(m::RheoModel, time (single value or array))
+    relaxmod(m::RheoModelClass, time (single value or array), parameters (array, named tupple or keyword parameters))
+        
+    examples:
+    
+    m = RheoModel(Maxwell, k=1., η=1)
+    g = relaxmod(m)
+    g([0,1,2])
+"""
 function relaxmod(m::RheoModel, t::Number)
     m._G(t)
 end
