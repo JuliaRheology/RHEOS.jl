0c1f0c2d680d03a202bcff8e012c8a3fefb1a4ed
Alexandre Kabla <ajk61@cam.ac.uk>
2020-05-26 17:47:25 +0100

    tests on PT model updated

--- test/models/poynting-thomson.jl
+++ test/models/poynting-thomson.jl
@@ -1,10 +1,14 @@
+println("===============================================")
+println("Testing poynting-thomson.jl")
+println("===============================================")
+
 function Fract_PT_G_reduce_SLS()
     dt = 0.001
     # NaN at 0.0 due to InverseLaplace so start at dt
     t = collect(dt:dt:10.0)
 
-    fracpt = Fract_PT.Ga(t, [2.0, 1.0, 1.0, 0.0, 0.5, 0.0])
-    stanSLS = SLS_PT.Ga(t, [2.0, 1.0, 0.5])
+    fracpt = relaxmod(Fract_PT, t, [2.0, 1.0, 1.0, 0.0, 0.5, 0.0])
+    stanSLS = relaxmod(SLS_PT, t, [2.0, 1.0, 0.5])
 
     all(i -> isapprox(fracpt[i], stanSLS[i]), eachindex(t))
 end
@@ -14,19 +18,21 @@ function FractSLS_PT_G_reduce_SLS()
     dt = 0.001
     t = collect(0.0:dt:10.0)
 
-    fracSLS = Ga(FractSLS_PT)(t, [2.0, 1.0, 1.0, 0.5])
-    stanSLS = SLS_PT.Ga(t, [2.0, 1.0, 0.5])
+    fracSLS = relaxmod(FractSLS_PT, t, [2.0, 1.0, 1.0, 0.5])
+    stanSLS = relaxmod(SLS_PT, t, [2.0, 1.0, 0.5])
 
     all(i -> isapprox(fracSLS[i], stanSLS[i]), eachindex(t))
 end
 @test FractSLS_PT_G_reduce_SLS()
 
+
+
 function Fract_PT_J_reduce_SLS(tol)
     dt = 0.001
     t = collect(0.0:dt:10.0)
 
-    fracpt = Ja(Fract_PT)(t, [2.0, 0.999, 1.0, 0.0, 0.5, 0.0])
-    stanSLS = SLS_PT.Ja(t, [2.0, 1.0, 0.5])
+    fracpt = creepcomp(Fract_PT, t, [2.0, 0.999, 1.0, 0.0, 0.5, 0.0])
+    stanSLS = creepcomp(SLS_PT, t, [2.0, 1.0, 0.5])
 
     all(i -> isapprox(fracpt[i], stanSLS[i], atol=tol), eachindex(t))
 end
@@ -36,19 +42,21 @@ function FractSLS_PT_J_reduce_SLS(tol)
     dt = 0.001
     t = collect(0.0:dt:10.0)
 
-    fracSLS = Ja(FractSLS_PT)(t, [2.0, 0.999, 1.0, 0.5])
-    stanSLS = SLS_PT.Ja(t, [2.0, 1.0, 0.5])
+    fracSLS = creepcomp(FractSLS_PT, t, [2.0, 0.999, 1.0, 0.5])
+    stanSLS = creepcomp(SLS_PT, t, [2.0, 1.0, 0.5])
 
     all(i -> isapprox(fracSLS[i], stanSLS[i], atol=tol), eachindex(t))
 end
 @test FractSLS_PT_J_reduce_SLS(tol)
 
+
+
 function Fract_PT_Gp_reduce_SLS()
     ω_step = 0.01
     chirp = collect(0.0:ω_step:10000.0)
 
-    fracpt = Fract_PT.Gpa(chirp, [2.0, 1.0, 1.0, 0.0, 0.5, 0.0])
-    stanSLS = SLS_PT.Gpa(chirp, [2.0, 1.0, 0.5])
+    fracpt = storagemod(Fract_PT, chirp, [2.0, 1.0, 1.0, 0.0, 0.5, 0.0])
+    stanSLS = storagemod(SLS_PT, chirp, [2.0, 1.0, 0.5])
 
     all(i -> isapprox(fracpt[i], stanSLS[i]), eachindex(chirp))
 end
@@ -58,19 +66,21 @@ function FractSLS_PT_Gp_reduce_SLS()
     ω_step = 0.01
     chirp = collect(0.0:ω_step:10000.0)
 
-    fracSLS = FractSLS_PT.Gpa(chirp, [2.0, 1.0, 1.0, 0.5])
-    stanSLS = SLS_PT.Gpa(chirp, [2.0, 1.0, 0.5])
+    fracSLS = storagemod(FractSLS_PT, chirp, [2.0, 1.0, 1.0, 0.5])
+    stanSLS = storagemod(SLS_PT, chirp, [2.0, 1.0, 0.5])
 
     all(i -> isapprox(fracSLS[i], stanSLS[i]), eachindex(chirp))
 end
 @test FractSLS_PT_Gp_reduce_SLS()
 
+
+
 function Fract_PT_Gpp_reduce_SLS()
     ω_step = 0.01
     chirp = collect(0.0:ω_step:10000.0)
 
-    fracpt = Fract_PT.Gppa(chirp, [2.0, 1.0, 1.0, 0.0, 0.5, 0.0])
-    stanSLS = SLS_PT.Gppa(chirp, [2.0, 1.0, 0.5])
+    fracpt = lossmod(Fract_PT, chirp, [2.0, 1.0, 1.0, 0.0, 0.5, 0.0])
+    stanSLS = lossmod(SLS_PT, chirp, [2.0, 1.0, 0.5])
 
     all(i -> isapprox(fracpt[i], stanSLS[i]), eachindex(chirp))
 end
@@ -80,20 +90,24 @@ function FractSLS_PT_Gpp_reduce_SLS()
     ω_step = 0.01
     chirp = collect(0.0:ω_step:10000.0)
 
-    fracSLS = FractSLS_PT.Gppa(chirp, [2.0, 1.0, 1.0, 0.5])
-    stanSLS = SLS_PT.Gppa(chirp, [2.0, 1.0, 0.5])
+    fracSLS = lossmod(FractSLS_PT, chirp, [2.0, 1.0, 1.0, 0.5])
+    stanSLS = lossmod(SLS_PT, chirp, [2.0, 1.0, 0.5])
 
     all(i -> isapprox(fracSLS[i], stanSLS[i]), eachindex(chirp))
 end
 @test FractSLS_PT_Gpp_reduce_SLS()
+
+
+
+
 
 function Fract_PT_G_reduce_Jeffreys(tol)
     dt = 0.001
     # NaN at 0.0 due to Gamma function so start at dt
     t = collect(dt:dt:10.0)
 
-    fracpt = Fract_PT.Ga(t, [2.0, 1.0, 1.0, 0.0, 0.5, 1.0])
-    stanJef = Jeffreys_PT.Ga(t, [2.0, 1.0, 0.5])
+    fracpt = relaxmod(Fract_PT, t, [2.0, 1.0, 1.0, 0.0, 0.5, 1.0])
+    stanJef = relaxmod(Jeffreys_PT, t, [2.0, 1.0, 0.5])
 
     all(i -> isapprox(fracpt[i], stanJef[i], atol=tol), eachindex(t))
 end
@@ -103,19 +117,21 @@ function FractJeffreys_PT_G_reduce_Jeffreys()
     dt = 0.001
     t = collect(0.0:dt:10.0)
 
-    fracJef = Ga(FractJeffreys_PT)(t, [2.0, 1.0, 0.0, 0.5])
-    stanJef = Jeffreys_PT.Ga(t, [2.0, 1.0, 0.5])
+    fracJef = relaxmod(FractJeffreys_PT, t, [2.0, 1.0, 0.0, 0.5])
+    stanJef = relaxmod(Jeffreys_PT, t, [2.0, 1.0, 0.5])
 
     all(i -> isapprox(fracJef[i], stanJef[i]), eachindex(t))
 end
 @test FractJeffreys_PT_G_reduce_Jeffreys()
 
+
+
 function Fract_PT_J_reduce_Jeffreys(tol)
     dt = 0.001
     t = collect(0.0:dt:10.0)
 
-    fracpt = Ja(Fract_PT)(t, [2.0, 0.999, 1.0, 0.0, 0.5, 1.0])
-    stanJef = Jeffreys_PT.Ja(t, [2.0, 1.0, 0.5])
+    fracpt = creepcomp(Fract_PT, t, [2.0, 0.999, 1.0, 0.0, 0.5, 1.0])
+    stanJef = creepcomp(Jeffreys_PT, t, [2.0, 1.0, 0.5])
 
     all(i -> isapprox(fracpt[i], stanJef[i], atol=tol), eachindex(t))
 end
@@ -125,19 +141,21 @@ function FractJeffreys_PT_J_reduce_Jeffreys(tol)
     dt = 0.001
     t = collect(0.0:dt:10.0)
 
-    fracJef = Ja(FractJeffreys_PT)(t, [2.0, 1.0, 0.001, 0.5])
-    stanJef = Jeffreys_PT.Ja(t, [2.0, 1.0, 0.5])
+    fracJef = creepcomp(FractJeffreys_PT, t, [2.0, 1.0, 0.001, 0.5])
+    stanJef = creepcomp(Jeffreys_PT, t, [2.0, 1.0, 0.5])
 
     all(i -> isapprox(fracJef[i], stanJef[i], atol=tol), eachindex(t))
 end
 @test FractJeffreys_PT_J_reduce_Jeffreys(tol)
 
+
+
 function Fract_PT_Gp_reduce_Jeffreys()
     ω_step = 0.01
     chirp = collect(0.0:ω_step:10000.0)
 
-    fracpt = Fract_PT.Gpa(chirp, [2.0, 1.0, 1.0, 0.0, 0.5, 1.0])
-    stanJef = Jeffreys_PT.Gpa(chirp, [2.0, 1.0, 0.5])
+    fracpt = storagemod(Fract_PT, chirp, [2.0, 1.0, 1.0, 0.0, 0.5, 1.0])
+    stanJef = storagemod(Jeffreys_PT, chirp, [2.0, 1.0, 0.5])
 
     all(i -> isapprox(fracpt[i], stanJef[i]), eachindex(chirp))
 end
@@ -147,19 +165,21 @@ function FractJeffreys_PT_Gp_reduce_Jeffreys()
     ω_step = 0.01
     chirp = collect(0.0:ω_step:10000.0)
 
-    fracJef = FractJeffreys_PT.Gpa(chirp, [2.0, 1.0, 0.0, 0.5])
-    stanJef = Jeffreys_PT.Gpa(chirp, [2.0, 1.0, 0.5])
+    fracJef = storagemod(FractJeffreys_PT, chirp, [2.0, 1.0, 0.0, 0.5])
+    stanJef = storagemod(Jeffreys_PT, chirp, [2.0, 1.0, 0.5])
 
     all(i -> isapprox(fracJef[i], stanJef[i]), eachindex(chirp))
 end
 @test FractJeffreys_PT_Gp_reduce_Jeffreys()
 
+
+
 function Fract_PT_Gpp_reduce_Jeffreys()
     ω_step = 0.01
     chirp = collect(0.0:ω_step:10000.0)
 
-    fracpt = Fract_PT.Gppa(chirp, [2.0, 1.0, 1.0, 0.0, 0.5, 1.0])
-    stanJef = Jeffreys_PT.Gppa(chirp, [2.0, 1.0, 0.5])
+    fracpt = lossmod(Fract_PT, chirp, [2.0, 1.0, 1.0, 0.0, 0.5, 1.0])
+    stanJef = lossmod(Jeffreys_PT, chirp, [2.0, 1.0, 0.5])
 
     all(i -> isapprox(fracpt[i], stanJef[i]), eachindex(chirp))
 end
@@ -169,8 +189,8 @@ function FractJeffreys_PT_Gpp_reduce_Jeffreys()
     ω_step = 0.01
     chirp = collect(0.0:ω_step:10000.0)
 
-    fracJef = FractJeffreys_PT.Gppa(chirp, [2.0, 1.0, 0.0, 0.5])
-    stanJef = Jeffreys_PT.Gppa(chirp, [2.0, 1.0, 0.5])
+    fracJef = lossmod(FractJeffreys_PT, chirp, [2.0, 1.0, 0.0, 0.5])
+    stanJef = lossmod(Jeffreys_PT, chirp, [2.0, 1.0, 0.5])
 
     all(i -> isapprox(fracJef[i], stanJef[i]), eachindex(chirp))
 end
