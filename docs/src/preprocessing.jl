7db6762939b483146f2527fa46dcf4e9d930b70f
Louis Kaplan <j.kiszle@gmail.com>
2020-12-31 18:37:53 +0000

    preprocessing done

--- docs/src/preprocessing.jl
+++ docs/src/preprocessing.jl
@@ -16,39 +16,52 @@ fig, ax = subplots(1, 1, figsize = (5, 5))
 ax.plot(foo.t, foo.ϵ, "--", marker = "o", markersize = 8, color = "blue")
 fig # for showing figure in docs
 
-# To downsample the full data set by taking every nth sample, the function [`resample`](@ref) is defined with negative argument. Similarly, to increase the sample rate, the function [`resample`](@ref) is defined with positive argument.
+# To downsample the full data set , the function [`resample`](@ref) is used with a `scale` argument less than 1. Similarly, to increase the sample rate, the `scale` argument should be greater than 1.
 
 #md # !!! note "Note"
-#md #     When 1 or -1 is defined, the function returns the original [`RheoTimeData`](@ref), whilst 0 is not accepted as a valid argument.
+#md #     When scaling, the argument can be an integer, floating point, or rational type. The function contains greater functionality and versatility than shown here. See the [`resample`](@ref) docstring for more information.
 
-#nb # Note: when 1 or -1 is defined, the function returns the original RheoTimeData, whilst 0 is not accepted as a valid argument.
+#nb # Note: when scaling, the argument can be an integer, floating point, or rational type. The function contains greater functionality and versatility than shown here. See the `resample` docstring for more information.
 
 ## Downsample
-foo_dsamp = resample(foo, -2)
+foo_dsamp = resample(foo, scale = 1//2)
 ## Upsample
-foo_usamp = resample(foo, 2)
+foo_usamp = resample(foo, scale = 2)
 
 ## Plotting
-fig, ax = subplots(1,2, figsize=(10,5))
+fig, ax = subplots(1, 2, figsize = (10, 5))
 ax[1].set_title("Downsampling")
 ax[1].plot(foo.t, foo.ϵ, "--", marker = "o", markersize = 8, color = "blue")
-ax[1].plot(foo_dsamp.t, foo_dsamp.ϵ, "--", marker = "x", markersize = 10, markeredgewidth=2, color = "orange")
+ax[1].plot(foo_dsamp.t, foo_dsamp.ϵ, "--", marker = "x", markersize = 10, markeredgewidth = 2, color = "orange")
 ax[2].set_title("Upsampling")
 ax[2].plot(foo.t, foo.ϵ, "--", marker = "o", markersize = 8, color = "blue")
-ax[2].plot(foo_usamp.t, foo_usamp.ϵ, "--", marker = "x", markersize = 10, markeredgewidth=2, color = "orange")
-fig
-
-# RHEOS also allows us to define time regions with different sampling rate. This requires the definition of the time boundaries to delimit where one sample rate finishes and another begins, it also includes the beginning and end points so the length of the time boundaries array argument should be one longer than the number of sample rates provided.
-
-## Variable sampling: Downsampling region [0,5] and upsampling [5,10]
-foo_samp = resample(foo, [-2,2], time_boundaries = [0.0, 5.0, 10.0])
-
-## Plotting
-fig, ax = subplots(1,1, figsize=(5,5))
-ax.plot(foo.t, foo.ϵ, "--", marker = "o", markersize = 8, color = "blue")
-ax.plot(foo_samp.t, foo_samp.ϵ, "--", marker = "x", markersize = 10, markeredgewidth = 2, color = "orange")
+ax[2].plot(foo_usamp.t, foo_usamp.ϵ, "--", marker = "x", markersize = 10, markeredgewidth = 2, color = "orange")
 fig
 
 # ## Cutting
 
-# RHEOS provides a dedicated function to remove the data outside a specified time interval.
+# RHEOS provides a dedicated function, [`resample`](@ref), to remove the data outside a specified time interval.
+
+foo_cut = cutting(foo, 2.0, 8.0)
+
+## Plotting
+fig, ax = subplots(1, 1, figsize = (5, 5))
+ax.plot(foo.t, foo.ϵ, "--", marker = "o", markersize = 8, color = "blue")
+ax.plot(foo_cut.t, foo_cut.ϵ, "--", marker = "x", markersize = 10, markeredgewidth = 2, color = "orange")
+fig
+
+# ## Smoothing
+
+# Lastly, RHEOS provides a smoothing function, [`smooth`](@ref). The first argument is the data to smooth and the second argument is the (very) approximate time scale of smoothing. (It uses Gaussian smoothing and can be thought of as a low pass filter for information occuring on time scales shorter than the 2nd argument). The padding can be changed using a keyword argument if desired, see [API](@ref) and [ImageFiltering.jl Documentation](https://juliaimages.github.io/ImageFiltering.jl/stable/) for more details.
+
+foo_s = timeline(t_start = 0, t_end = 10, step = 0.02)
+foo_s = strainfunction(foo_s, t -> 3*sin(t))
+noise = strainfunction(foo_s, t -> rand())
+
+foo_noisy = foo_s + noise
+foo_smooth = smooth(foo_noisy, 1)
+
+fig, ax = subplots(1, 1, figsize = (5, 5))
+ax.plot(foo_noisy.t, foo_noisy.ϵ, color = "blue")
+ax.plot(foo_smooth.t, foo_smooth.ϵ, color = "orange")
+fig
