0a565eaaff9d37aaf278a3423c8eaa184a6c3119
Louis Kaplan <j.kiszle@gmail.com>
2021-01-02 09:50:36 +0000

    model KV done

--- docs/src/model_zener.jl
+++ docs/src/model_zener.jl
@@ -2,20 +2,41 @@
 #md # [![](https://mybinder.org/badge_logo.svg)](@__BINDER_ROOT_URL__/notebooks/model_zener.ipynb)
 #md # [![](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/notebooks/model_zener.ipynb)
 
+using RHEOS
+## include a helper function for plotting
+include("assets/plothelper.jl");
+#-
+
+
+
 # #### Constitutive Equation
 # ```math
+#
+# ```
+
+# ```math
+#
+# ```
 
 # #### Relaxation Modulus
 # ```math
+#
+# ```
 
 # #### Creep Modulus
 # ```math
+#
+# ```
 
 # #### Storage Modulus
 # ```math
+#
+# ```
 
 # #### Loss Modulus
 # ```math
+#
+# ```
 
 # # Fractional SLS (Zener)
 
