9aba350ccc8f60054f233f819df8d0e212c95ba5
Louis Kaplan <j.kiszle@gmail.com>
2021-01-06 17:57:35 +0000

    fix indexweight function signature and docstring to reflect updated usage

--- src/processing.jl
+++ src/processing.jl
@@ -102,7 +102,7 @@ end
 
 
 """
-    indexweight(self::RheoTimeData, elperiods::Union{Vector{K}, K}; time_boundaries::Union{Nothing, Vector{T}} = nothing, includelast=true) where {K<:Integer,T<:Real}
+    indexweight(self::RheoTimeData; elperiods::Vector{K}, time_boundaries::Union{Nothing, Vector{T}} = nothing, includelast=true) where {K<:Integer,T<:Real}
 
 This function returns array indices (i.e. an array of integers) which can be sent to the `modelfit`, `modelstepfit` or `modeldiffeqfit` functions
 to provide a weighted fitting whilst maintaining constant sample-rate.
@@ -111,13 +111,12 @@ Note that `time_boundaries` must have one more entry than `elperiods` so that al
 
 `indexweight` can underweight indices or overweight them. If `elperiods` in a given boundary is negative, every `abs(n)` index will be used, where `n` is the `elperiod`
 corresponding to a given boundary. If `n` is positive, then indicies will be duplicated `n` times such that they are given a higher weighting during the fitting procedure.
-If `time_boundaries` are not specified, resampling is applied to the whole set of data. If number of elements per period (`elperiods`) is `1` or `-1` it returns the original
-indicies for that boundary, whilst `0` is not accepted as a valid argument for `elperiods`.
+If number of elements per period (`elperiods`) is `1` or `-1` it returns the original indicies for that boundary, whilst `0` is not accepted as a valid argument for `elperiods`.
 
 The last element may or may not be included. By default the last element is forced to be included
 but this can be negated by providing the keyword argument `includelast=false`.
 """
-function indexweight(self::RheoTimeData; elperiods::Union{Vector{K}, K}, time_boundaries::Union{Nothing, Vector{T}} = nothing, includelast=true) where {K<:Integer,T<:Real}
+function indexweight(self::RheoTimeData; elperiods::Vector{K}, time_boundaries::Union{Nothing, Vector{T}} = nothing, includelast=true) where {K<:Integer,T<:Real}
     # get element-wise boundaries
     if isnothing(time_boundaries)
         boundaries = [1,length(self.t)]
