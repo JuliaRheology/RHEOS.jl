74a2d0141d920d3ca6f4f45ce6e3a9b519ab8a79
Sanjan Das <33346052+Sanjan611@users.noreply.github.com>
2021-05-28 20:20:26 +0530

    some changes to RHEOS.jl to add the MittagLeffler. Now the sameas in the master branch

--- src/RHEOS.jl
+++ src/RHEOS.jl
@@ -4,12 +4,16 @@ __precompile__(true)
 module RHEOS
 
 # installed from Julia package repository
-using InverseLaplace
 using NLopt
 using JLD2
 using DataStructures
 using FunctionWrappers: FunctionWrapper
 using Dierckx
+
+# useful for the various model functions
+using InverseLaplace
+import MittagLeffler: mittleff as mittlefforiginal
+import SpecialFunctions: gamma
 
 import DSP.conv
 
@@ -32,26 +36,12 @@ const IntOrNone = Union{Integer, Nothing}
 const RheovecOrNone = Union{Vector{RheoFloat}, Nothing}
 
 ######################################################################
-# Additional exports for experiments with modelpredict function
-
-export singularity_offset
-
-# Base.jl
-export singularitytest, constantcheck
-export derivBD, derivCD
-export doublederivCD
-export boltzconvolve, boltzintegral_sing, boltzintegral_nonsing
-
-# definitions.jl
-export _Ga, _Ja
-
-######################################################################
 export RheoFloat
 
 # definitions.jl
 export RheoLogItem, RheoLog, rheologrun, showlog
 export RheoTimeData, RheoFreqData
-export RheoTimeDataType, RheoFreqDataType, check_time_data_consistency
+export rheotimedatatype, rheofreqdatatype, check_time_data_consistency
 export LoadingType, strain_imposed, stress_imposed
 export hastime, hasstress, hasstrain
 export TimeDataType, time_only, strain_only, stress_only, strain_and_stress
@@ -66,7 +56,7 @@ export importcsv, exportcsv#, savedata, loaddata, savemodel, loadmodel
 
 # models.jl
 export null_modulus
-export Springpot, Springpot_i, Spring, Dashpot
+export Springpot, Spring, Dashpot
 export Fract_Maxwell, FractS_Maxwell, FractD_Maxwell, Maxwell
 export Fract_KelvinVoigt, FractS_KelvinVoigt, FractD_KelvinVoigt, KelvinVoigt
 export Fract_Zener, FractSLS_Zener, SLS_Zener, FractJeffreys_Zener, Jeffreys_Zener, FractSolid
@@ -89,13 +79,10 @@ export dynamicmodelfit, dynamicmodelpredict
 export Interface
 export AFM, Tweezers
 
-#decompose.jl
-export decompose
-
 ######################################################
 # bundled dependencies from rheos-cambridge forked repos
-MittLeffLiteDir = joinpath(@__DIR__, "..", "deps", "MittLeffLite", "MittLeffLite.jl")
-include(MittLeffLiteDir)
+#MittLeffLiteDir = joinpath(@__DIR__, "..", "deps", "MittLeffLite", "MittLeffLite.jl")
+#include(MittLeffLiteDir)
 
 include("base.jl")
 include("definitions.jl")
@@ -104,6 +91,115 @@ include("modeldatabase.jl")
 include("datagen.jl")
 include("processing.jl")
 include("interface.jl")
-include("decompose.jl")
 
 end
+# OLD--------------------------
+# #!/usr/bin/env julia
+# __precompile__(true)
+
+# module RHEOS
+
+# # installed from Julia package repository
+# using InverseLaplace
+# using NLopt
+# using JLD2
+# using DataStructures
+# using FunctionWrappers: FunctionWrapper
+# using Dierckx
+
+# import DSP.conv
+
+# # Base and stdlib imports
+# import Base: +, -, *,|
+# import DelimitedFiles: readdlm, writedlm
+
+# ######################################################################
+
+# # This defines the data type for all arrays, parameters and processing
+# # it is defined as a const to avoid performance penalties.
+# # See julia docs for more info on this.
+# const RheoFloat = Float64
+
+# # Singularity approximation constant
+# const singularity_offset = 10.0
+
+# # convenience data types used as in many function parameters
+# const IntOrNone = Union{Integer, Nothing}
+# const RheovecOrNone = Union{Vector{RheoFloat}, Nothing}
+
+# ######################################################################
+# # Additional exports for experiments with modelpredict function
+
+# export singularity_offset
+
+# # Base.jl
+# export singularitytest, constantcheck
+# export derivBD, derivCD
+# export doublederivCD
+# export boltzconvolve, boltzintegral_sing, boltzintegral_nonsing
+
+# # definitions.jl
+# export _Ga, _Ja
+
+# ######################################################################
+# export RheoFloat
+
+# # definitions.jl
+# export RheoLogItem, RheoLog, rheologrun, showlog
+# export RheoTimeData, RheoFreqData
+# export RheoTimeDataType, RheoFreqDataType, check_time_data_consistency
+# export LoadingType, strain_imposed, stress_imposed
+# export hastime, hasstress, hasstrain
+# export TimeDataType, time_only, strain_only, stress_only, strain_and_stress
+# export FreqDataType, invalid_freq_data, freq_only, with_modulus
+# export RheoModelClass, RheoModel, model_parameters
+# export rheoconv, invLaplace
+# export freeze_params
+# export relaxmod, creepcomp, storagemod, lossmod, dynamicmod
+
+# # IO.jl
+# export importcsv, exportcsv#, savedata, loaddata, savemodel, loadmodel
+
+# # models.jl
+# export null_modulus
+# export Springpot, Springpot_i, Spring, Dashpot
+# export Fract_Maxwell, FractS_Maxwell, FractD_Maxwell, Maxwell
+# export Fract_KelvinVoigt, FractS_KelvinVoigt, FractD_KelvinVoigt, KelvinVoigt
+# export Fract_Zener, FractSLS_Zener, SLS_Zener, FractJeffreys_Zener, Jeffreys_Zener, FractSolid
+# export Fract_PT, FractSLS_PT, SLS_PT, Jeffreys_PT, FractJeffreys_PT
+# export BurgersLiquid
+# export SLS2, PowerLawPlateau
+
+# # datagen.jl
+# export timeline
+# export strainfunction, stressfunction
+# export hstep, ramp, stairs, square, sawtooth, triangle
+# export frequencyspec
+
+# # processing.jl
+# export resample, indexweight, cutting, smooth, extract
+# export modelfit, modelpredict, modelstepfit, modelsteppredict
+# export dynamicmodelfit, dynamicmodelpredict
+
+# #interface.jl
+# export Interface
+# export AFM, Tweezers
+
+# #decompose.jl
+# export decompose
+
+# ######################################################
+# # bundled dependencies from rheos-cambridge forked repos
+# MittLeffLiteDir = joinpath(@__DIR__, "..", "deps", "MittLeffLite", "MittLeffLite.jl")
+# include(MittLeffLiteDir)
+
+# include("base.jl")
+# include("definitions.jl")
+# include("IO.jl")
+# include("modeldatabase.jl")
+# include("datagen.jl")
+# include("processing.jl")
+# include("interface.jl")
+# include("decompose.jl")
+
+# end
