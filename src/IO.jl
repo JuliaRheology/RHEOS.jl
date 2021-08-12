4e9f577e87483dbf4b6e5897ef0e5027c7abfcb7
Alexandre Kabla <ajk61@cam.ac.uk>
2021-08-12 16:31:08 +0100

    Minor changes

--- src/IO.jl
+++ src/IO.jl
@@ -46,16 +46,25 @@ end
     importcsv(filepath::String; t_col::IntOrNone = nothing, σ_col::IntOrNone = nothing, ϵ_col::IntOrNone = nothing, ω_col::IntOrNone = nothing, Gp_col::IntOrNone = nothing, Gpp_col::IntOrNone = nothing, delimiter=',', header=false)
 
 Load data from a CSV file (two/three columns, comma separated by default but
-delimiter can be specified in the `delimiter` keyword argument). Arguments must be
-identified by providing the number of the column in which they are contained.
+delimiter can be specified in the `delimiter` keyword argument). 
 
-Can be used to construct either a RheoTimeData instance or a RheoFreqData
-instance. Function detects whether time or frequency has been included
+The function can be used to construct either a RheoTimeData instance or a RheoFreqData
+instance, depending on the parameters provided or column names;the function detects whether time or frequency has been included
 and proceeds accordingly. For oscillatory data, all three columns (Gp, Gpp, Frequency)
 must be provided. For regular viscoelastic data only time, or time-stress, or time-strain or
 time-stress-strain data can be provided.
+
+Column can be specified in the csv file by providing their values or header name as keyword parameters. 
+
+`importcsv("filename.csv", time=1, strain=2, stress=3)` loads a `RheoTimeData` from a csv file with time in the first column, strain in the second, and stress in the third.
+
+`importcsv("filename.csv", omega="Frequency", Gp="Storage", Gpp="Loss")` would detect the column numbers by reading the headers on the csv file.
+
+If no information is provided, it would try to detect columns from header names, expecting standard names. Some of the recognised keywords are `time`, `stress`, `strain`, `frequency`, `storage modulus`, `loss modulus`.
+
+If the csv file contains headers, but it is prefered to indicate columns by their numbers rather than header strings, the keyword `header` must be set to `true`.
 """
-function importcsv(filepath::String; cols=nothing, delimiter = ',', header = false, comment = "Imported from csv file", savelog = true, kwargs...)
+function importcsv(filepath::String; delimiter = ',', header = false, comment = "Imported from csv file", savelog = true, kwargs...)
 # Convert parameter names to standard values.
 	cols=symbol_to_unicode(kwargs.data)
 
--- src/definitions.jl
+++ src/definitions.jl
@@ -993,7 +993,7 @@ end
 `RheoModel` represents a rheological model with set parameters. They are obtained by fitting a model to data using `modelfit`, 
 or by specialising the relevant RheoModelClass by prescribing its parameters. Parameters can be provided as a named tuple or keyword arguments.
 
-`RheoModel` objects can then be use to simulate the response to an arbitrary input using `modelpredict`, and access the values of the moduli functions.
+`RheoModel` objects can then be used to simulate the response to an arbitrary input using `modelpredict`, and access the values of the moduli functions.
 
 # Example
 ```@example
