014785a4ac01a1ff6fddd9aa146832a97a824f15
Documenter.jl <documenter@juliadocs.github.io>
2024-03-03 15:05:12 +0000

    build based on ea942d2

--- dev/notebooks/assets/plothelper.jl
+++ dev/notebooks/assets/plothelper.jl
@@ -1,12 +1,12 @@
 using PyPlot
-using PyCall
+# using PyCall
 
 # need a separate Python helper file for
 # centered lower figure as colon operator 
 # not implemented fully implemented in PyCall
-gs = pyimport("matplotlib.gridspec")
-pushfirst!(PyVector(pyimport("sys")."path"), joinpath("assets"))
-plothelper = pyimport("plothelper")
+# gs = pyimport("matplotlib.gridspec")
+# pushfirst!(PyVector(pyimport("sys")."path"), joinpath("assets"))
+# plothelper = pyimport("plothelper")
 
 function plotmodel(modelvect; ymaxG = nothing, ymaxJ = nothing)
 
@@ -21,13 +21,14 @@ function plotmodel(modelvect; ymaxG = nothing, ymaxJ = nothing)
 
     colplot = ["red", "#dc7633", "#229954", "#d4ac0d", "blue"]
 
-    fig, spec, ax1, ax2, ax3 = plothelper.get_fig_axes()
-    spec.update(wspace = 0.25,
-                hspace = 0.25,
-                top = 0.96,
-                bottom = 0.07,
-                left = 0.07,
-                right = 0.98)
+    fig, axs = subplots(3,1, figsize=(7,20))
+    ax1, ax2, ax3 = axs
+    # spec.update(wspace = 0.25,
+    #             hspace = 0.25,
+    #             top = 0.96,
+    #             bottom = 0.07,
+    #             left = 0.07,
+    #             right = 0.98)
 
     for i = 1:1:length(modelvect)
         #Relaxation modulus
@@ -50,8 +51,8 @@ function plotmodel(modelvect; ymaxG = nothing, ymaxJ = nothing)
     isnothing(ymaxJ) ? ax1.set_ylim(bottom = -dt, top = ymaxG) : ax1.set_ylim(bottom = -dt)
     ax1.tick_params("both", labelsize = 9)
     ax1.tick_params(axis="x", which="minor", bottom=true)
-    ax1.grid(b=true, alpha = 0.2, axis="y", which="major")
-    ax1.grid(b=true, alpha = 0.2, axis="x", which="major")
+    ax1.grid(alpha = 0.2, axis="y", which="major")
+    ax1.grid(alpha = 0.2, axis="x", which="major")
 
     ax2.set_xlabel("Time", fontsize = 10)
     ax2.set_ylabel("Strain", fontsize = 10)
@@ -60,8 +61,8 @@ function plotmodel(modelvect; ymaxG = nothing, ymaxJ = nothing)
     isnothing(ymaxJ) ? ax2.set_ylim(bottom = -dt, top = ymaxJ) : ax2.set_ylim(bottom = -dt)
     ax2.tick_params("both", labelsize = 9)
     ax2.tick_params(axis="x", which="minor", bottom=true)
-    ax2.grid(b=true, alpha = 0.2, axis="y", which="major")
-    ax2.grid(b=true, alpha = 0.2, axis="x", which="major")
+    ax2.grid(alpha = 0.2, axis="y", which="major")
+    ax2.grid(alpha = 0.2, axis="x", which="major")
 
     dω = frequencyspec(ω_start = 1e-2, ω_end = 1e2, logstep = log10(1e2/1e-2)/30)
 
@@ -75,8 +76,8 @@ function plotmodel(modelvect; ymaxG = nothing, ymaxJ = nothing)
     ax3.set_xlabel("Frequency", fontsize = 10)
     ax3.set_ylabel("Storage (—) and Loss (- -) moduli", fontsize = 10)
     ax3.tick_params("both", labelsize = 9)
-    ax3.grid(b=true, alpha = 0.2, axis="y", which="major")
-    ax3.grid(b=true, alpha = 0.2, axis="x", which="major")
+    ax3.grid(alpha = 0.2, axis="y", which="major")
+    ax3.grid(alpha = 0.2, axis="x", which="major")
     ax3.set_title("Frequency response", fontsize = 10)
 
     return fig
