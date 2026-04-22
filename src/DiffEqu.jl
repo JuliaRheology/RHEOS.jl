ee5510b1d4bf3637ef625e9246441982e46f73a6
Lorenzo Fonnesu <75188388+Fonzy-01@users.noreply.github.com>
2026-04-22 16:54:14 +0200

    Added function to predict just one step

--- src/DiffEqu.jl
+++ src/DiffEqu.jl
@@ -524,6 +524,103 @@ function _modelpredictGL(data::RheoTimeData, equation ,diff_method)
     return(computed, input, t)
 end
 
+function _modelpredictGL_step!(data::RheoTimeData, equation, diff_method, controlled, new_element, n)
+
+    # Define the derivative function and the input data based on the type of data provided
+    if diff_method=="BD"
+        deriv = derivBD
+    elseif diff_method=="CD"
+        deriv = derivCD
+    end
+
+    if (controlled == "strain")
+        unknown = equation.rightde
+        dependency = equation.leftde
+        data.ϵ[n] = new_element
+        input = data.ϵ
+        history = data.σ[1:(n-1)]
+    elseif (controlled == "stress")
+        unknown = equation.leftde
+        dependency = equation.rightde
+        data.σ[n] = new_element
+        input = data.σ
+        history = data.ϵ[1:(n-1)]
+    end
+
+    input_tmp = input[1:n]    # Cut the input up to n, since we are only computing one time step
+
+    dt = data.t[2] - data.t[1]
+    deriv_data = deriv(input_tmp, data.t[1:n])   # First derivative of the input data
+
+    prob = NumDiffProblem(dt=dt, order=0.5, n=n, method=GL())
+    ws = init_workspace(prob)
+
+    denominator = 0.0
+    rhs = 0.0
+
+    # Compute the right-hand side of the equation based on the known terms
+    for c in dependency
+        if c.order == 0.0
+            rhs += c.coef * input_tmp[n]
+        elseif c.order == 1.0
+            rhs += c.coef * deriv_data[n]
+        else
+            update_order!(prob, ws, c.order)
+            compute!(prob.method, ws, input_tmp, prob)
+            rhs += c.coef * (ws.deriv)[n]
+        end
+    end
+
+    # Compute the denominator and the binomial coefficient weights for non-integer orders
+    # Store the binomial coefficients in a dictionary to avoid redundant calculations for repeated orders
+    bin_coeffs = Dict{Float64, Vector{Float64}}()
+    for c in unknown
+        if c.order == 0.0
+            denominator += c.coef
+        elseif c.order == 1.0
+            denominator += c.coef / dt
+        elseif round(c.order) != c.order && !haskey(bin_coeffs, c.order)
+            denominator += c.coef / (dt^c.order)
+            bin_coeffs[c.order] = zeros(n)
+            
+            update_order!(prob, ws, c.order)
+            generate_weights!(prob.method, prob, ws)
+            bin_coeffs[c.order] = ws.weights
+        else
+            denominator += c.coef / (dt^c.order)
+        end
+    end
+
+    # Compute the first value of the computed array
+    inv_denominator = 1.0 / denominator
+
+    # Compute the rest of the values by updating the rhs with previous computed values for the unknown terms and dividing by the denominator
+    for c in unknown
+        if c.order == 1.0
+            rhs += history[n-1] * c.coef / dt
+
+        elseif round(c.order) != c.order
+            weights = bin_coeffs[c.order]
+            coef_over_dt = c.coef / (dt^c.order) 
+
+            conv_sum = 0.0
+            @inbounds @simd for k in 1:(n-1)
+                conv_sum += weights[k+1] * history[n-k]
+            end
+
+            rhs -= coef_over_dt * conv_sum
+
+        elseif c.order != 0.0
+            println("Order not implemented yet: $c")
+        end
+    end
+
+    new_value = rhs * inv_denominator
+
+    return new_value
+end
+
+
 function _modelpredictFFT_stress(data::RheoTimeData, equation)
 
     # Define the input data based on the type of data provided
